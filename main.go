bff67fc212c9f5b44ef520ef1968d85075a69ad2
yuanyangen <yuanyangen126.com>
2017-09-03 19:30:02 +0800

    one version

--- main.go
+++ main.go
@@ -1,33 +1,82 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"http_bench"
+	"os"
+	"runtime"
+	"runtime/pprof"
+	"flag"
 )
 
 //从命令行中获取参数，并将参数写入benchmark的app中
 func main() {
-	app := http_bench.BenchmarkApp
-	app.Concurrent = flag.Int("c", 0, "concurrent")
-	app.Cookie = flag.String("cookie", "", "cookie")
-	app.PostField = flag.String("post", "", "post info")
-	url := flag.String("url", "", "target url")
+	//app.Concurrent = flag.Int("c", 0, "concurrent")
+	//app.Number = flag.Uint64("n", 0, "number")
+	//flag.Parse()
+	//	startCPUProfile()
+	//	startMemProfile()
+	//	startBlockProfile()
+	app := http_bench.GetBenchApp()
+	c := flag.Int("c", 1, "concurrent")
+	u := flag.String("u", "", "url")
+	f := flag.String("f", "", "filePath")
+	d := flag.Int("d", 10, "duration")
 	flag.Parse()
-	if "" == *url {
-		fmt.Println("usage:\n")
+	app.SetConcurrent(*c)
+	app.SetFilePath(*f)
+	app.SetUrl(*u)
+	app.SetDuration(*d)
+	app.Start()
+	//	stopCPUProfile()
+	//	stockMemProfile()
+	//	stopBlockProfile()
+}
+func startCPUProfile() {
+	f, err := os.OpenFile("/tmp/go_pprof_cpu.out", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can not create cpu profile output file: %s", err)
+		return
 	}
-
-	app.Bench(*url)
-	fmt.Println(*app.Result)
-
+	if err := pprof.StartCPUProfile(f); err != nil {
+		fmt.Fprintf(os.Stderr, "Can not start cpu profile: %s", err)
+		f.Close()
+		return
+	}
 }
 
-//对http请求的封装，能够模拟get请求， post请求， 带上cookie
+func stopCPUProfile() {
+	pprof.StopCPUProfile()
+}
 
-//对transport的封装，在底层能够获取信息，
+func startMemProfile() {
+	//每多少内存进行一次检查内存的记录， 默认是512k
+	runtime.MemProfileRate = 1024 * 64
+}
+func stockMemProfile() {
+	f, err := os.OpenFile("/tmp/go_pprof_mem.out", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can not create mem profile output file: %s", err)
+		return
+	}
+	if err = pprof.WriteHeapProfile(f); err != nil {
+		fmt.Fprintf(os.Stderr, "Can not write %s: ", err)
+	}
+	f.Close()
+}
 
-//能够支持长连接，
-//
-
-//
+func startBlockProfile() {
+	//如果我们不显式的使用runtime.SetBlockProfileRate函数设置取样间隔，那么取样间隔就为1
+	runtime.SetBlockProfileRate(1)
+}
+func stopBlockProfile() {
+	f, err := os.OpenFile("/tmp/go_pprof_block.out", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can not create mem profile output file: %s", err)
+		return
+	}
+	if err = pprof.Lookup("block").WriteTo(f, 0); err != nil {
+		fmt.Fprintf(os.Stderr, "Can not write %s:", err)
+	}
+	f.Close()
+}
--- src/http_bench/benchmark.go
+++ src/http_bench/benchmark.go
@@ -3,59 +3,143 @@ package http_bench
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"strings"
+	"sync"
 	"time"
 )
 
 //对http请求的封装，能够模拟get请求， post请求， 带上cookie
 type benchmarkApp struct {
-	Concurrent *int
-	Cookie     *string
-	PostField  *string
-	Result     *string
+	param     *Param
+	res       *result
+	wg        *sync.WaitGroup
+	msgChan   chan int
+	UrlG      UrlGenerator
+	transport *http.Transport
+}
+type Param struct {
+	Concurrent int
+	Number     uint64
+	FilePath   string
+	TargetUrl   string
+	Dura       time.Duration
+	Wd         bool
+}
+type result struct {
+	total     uint64
+	failed    uint64
+	bizFailed uint64
 }
 
-var BenchmarkApp = &benchmarkApp{}
 
-func (this *benchmarkApp) Bench(url string) {
-	defaultRequest.SetUrl(url)
-	if "" != *this.PostField {
-		defaultRequest.postField = *this.PostField
+const TYPE_TOTAL = 0
+const TYPE_FAILED = 1
+const TYPE_WD_BIZ_FAILED = 2
+
+func (app *benchmarkApp) Start() {
+	app.initApp()
+	app.Execute()
+}
+
+func GetBenchApp() *benchmarkApp {
+	app := &benchmarkApp{}
+	app.msgChan = make(chan int, 1024)
+	app.param = &Param{}
+	app.transport = &http.Transport{}
+	return app
+}
+
+func (app * benchmarkApp) SetConcurrent(c int) {
+	app.param.Concurrent = c
+}
+
+func (app * benchmarkApp) SetDuration(d int) {
+	app.param.Dura = time.Duration(d)
+}
+
+func (app * benchmarkApp) SetUrl(url string) {
+	app.param.TargetUrl = url
+}
+
+func (app * benchmarkApp) SetFilePath(fp string) {
+	app.param.FilePath = fp
+}
+
+func (app *benchmarkApp) initApp() {
+	if app.param.FilePath != "" {
+		app.UrlG = GetNewFileUrlGenerator(app.param.FilePath)
+	} else if app.param.TargetUrl != "" {
+		app.UrlG = GetSpecificUrlGenerator(app.param.TargetUrl)
+	} else {
+		displayErrors("url not specified\n")
 	}
-	if "" != *this.Cookie {
-		defaultRequest.cookieJar = *this.Cookie
-	}
-	fmt.Println("11111111111111")
 
 }
 
 //运行一个curl的对象，并将返回信息返回
-func (this *benchmarkApp) Execute() {
-	//设置一个channel作为超时使用
-	channel := make(chan string, 1)
-	httpRequest := defaultRequest.getHttpStream()
-	go func(httpRequest string) {
-		conn := getConn()
-		_, err := conn.Write([]byte(httpRequest))
-		if err != nil {
-		}
-		tmpInfo, _ := ioutil.ReadAll(conn)
-		//这里应该将输出的字符串整理成response对象
-		defaultResp.processResponse(string(tmpInfo))
-		if defaultResp.header == "200" {
-			fmt.Println(defaultResp.header)
-		}
-		channel <- defaultResp.header
-	}(httpRequest)
-	//如果超时, 返回的响应头中的header为timeout
-	select {
-	case <-channel:
-		{
-			return
-		}
-	case <-time.After(time.Second * defaultRequest.timeout):
-		{
-			defaultResp.header = "timeout"
+func (app *benchmarkApp) Execute() {
+	go app.receiveResult()
+	for i := 0; i < app.param.Concurrent; i++ {
+		go app.doOneWorker()
+	}
+	time.Sleep(app.param.Dura * time.Second)
+	app.dispalyResult()
+}
+
+func (app *benchmarkApp) dispalyResult() {
+	fmt.Printf("qps: %d\n", app.res.total/uint64(app.param.Dura))
+	fmt.Printf("total request: %d\n", app.res.total)
+	fmt.Printf("failed request: : %d\n", app.res.failed)
+	fmt.Printf("total traget urls : %d\n", app.res.total/uint64(app.param.Dura))
+}
+
+func (app *benchmarkApp) receiveResult() {
+	app.res = &result{}
+	for {
+		msg := <-app.msgChan
+		if msg == TYPE_TOTAL {
+			app.res.total++
+		} else if msg == TYPE_WD_BIZ_FAILED {
+			app.res.bizFailed++
+		} else if msg == TYPE_FAILED {
+			app.res.failed++
 		}
 	}
-	return
+}
+
+func (app *benchmarkApp) doOneWorker() {
+	//trans := app.transport
+	trans := &http.Transport{}
+	for {
+		app.doOneRequest(trans)
+	}
+}
+
+func (app *benchmarkApp) doOneRequest(trans *http.Transport) {
+	targetUrl := app.getTargetUrl(app.UrlG)
+	client := &http.Client{Transport: trans}
+	req, _ := http.NewRequest(http.MethodGet, targetUrl, nil)
+	resp, err := client.Do(req)
+
+	if err != nil {
+		app.msgChan <- TYPE_FAILED
+		return
+	}
+	if resp.StatusCode != 200 {
+		app.msgChan <- TYPE_FAILED
+		return
+	}
+	r, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		app.msgChan <- TYPE_FAILED
+	}
+	if app.param.Wd && !strings.HasPrefix(string(r), "{\"errno\":0") {
+		app.msgChan <- TYPE_WD_BIZ_FAILED
+	}
+	app.msgChan <- TYPE_TOTAL
+}
+
+func (app *benchmarkApp) getTargetUrl(urlG UrlGenerator) string {
+	return urlG.GetUrl()
 }
