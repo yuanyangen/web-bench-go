c57a53c64fbfdfb5f5905be204f79fd59afc93dd
yuanyangen <yuanyangen126.com>
2017-09-03 23:38:58 +0800

    performace optimize

--- src/http_bench/benchmark.go
+++ src/http_bench/benchmark.go
@@ -4,28 +4,30 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
-	"sync"
-	"time"
 	"os"
 	"os/signal"
+	"strings"
+	"time"
 )
 
-//对http请求的封装，能够模拟get请求， post请求， 带上cookie
 type benchmarkApp struct {
-	param     *Param
-	res       *result
-	wg        *sync.WaitGroup
-	msgChan   chan int
-	UrlG      UrlGenerator
-	transport *http.Transport
+	param   *Param
+	res     *result
+	UrlG    UrlGenerator
+	workers []*worker
+}
+type worker struct {
+	param    *Param
+	res      *result
+	UrlG     UrlGenerator
+	StopFlag bool //是否停止？
 }
 type Param struct {
 	Concurrent int
 	Number     uint64
 	FilePath   string
-	TargetUrl   string
-	Dura       time.Duration
+	TargetUrl  string
+	dur        time.Duration
 	Wd         bool
 }
 type result struct {
@@ -34,11 +36,6 @@ type result struct {
 	bizFailed uint64
 }
 
-
-const TYPE_TOTAL = 0
-const TYPE_FAILED = 1
-const TYPE_WD_BIZ_FAILED = 2
-
 func (app *benchmarkApp) Start() {
 	app.initApp()
 	app.Execute()
@@ -46,25 +43,25 @@ func (app *benchmarkApp) Start() {
 
 func GetBenchApp() *benchmarkApp {
 	app := &benchmarkApp{}
-	app.msgChan = make(chan int, 1024)
 	app.param = &Param{}
-	app.transport = &http.Transport{}
+	app.workers = make([]*worker, 0)
+	app.res = &result{}
 	return app
 }
 
-func (app * benchmarkApp) SetConcurrent(c int) {
+func (app *benchmarkApp) SetConcurrent(c int) {
 	app.param.Concurrent = c
 }
 
-func (app * benchmarkApp) SetDuration(d int) {
-	app.param.Dura = time.Duration(d)
+func (app *benchmarkApp) SetDuration(d int) {
+	app.param.dur = time.Duration(d)
 }
 
-func (app * benchmarkApp) SetUrl(url string) {
+func (app *benchmarkApp) SetUrl(url string) {
 	app.param.TargetUrl = url
 }
 
-func (app * benchmarkApp) SetFilePath(fp string) {
+func (app *benchmarkApp) SetFilePath(fp string) {
 	app.param.FilePath = fp
 }
 
@@ -81,74 +78,72 @@ func (app *benchmarkApp) initApp() {
 
 //运行一个curl的对象，并将返回信息返回
 func (app *benchmarkApp) Execute() {
-	go app.receiveResult()
 	for i := 0; i < app.param.Concurrent; i++ {
-		go app.doOneWorker()
+		wk := &worker{}
+		wk.param = app.param
+		wk.res = &result{}
+		wk.StopFlag = false
+		wk.UrlG = app.UrlG
+		app.workers = append(app.workers, wk)
+		go wk.startWorker()
 	}
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt, os.Kill)
 	select {
-	case <- signalChan: {
+	case <-signalChan:
+	case <-time.After(app.param.dur * time.Second):
 	}
-	case <- time.After(app.param.Dura * time.Second) : {
-	}
+	app.stopApp()
+}
+
+func (app *benchmarkApp) stopApp() {
+	for _, wk := range app.workers {
+		wk.StopFlag = true
+		app.res.total += wk.res.total
+		app.res.failed += wk.res.failed
+		app.res.bizFailed += wk.res.bizFailed
 	}
 	app.dispalyResult()
 }
 
 func (app *benchmarkApp) dispalyResult() {
-	fmt.Printf("qps: %d\n", app.res.total/uint64(app.param.Dura))
+	fmt.Printf("qps: %d\n", app.res.total/uint64(app.param.dur))
 	fmt.Printf("total request: %d\n", app.res.total)
 	fmt.Printf("failed request: : %d\n", app.res.failed)
-	fmt.Printf("total traget urls : %d\n", app.res.total/uint64(app.param.Dura))
+	fmt.Printf("total traget urls : %d\n", app.res.total/uint64(app.param.dur))
 }
 
-func (app *benchmarkApp) receiveResult() {
-	app.res = &result{}
-	for {
-		msg := <-app.msgChan
-		if msg == TYPE_TOTAL {
-			app.res.total++
-		} else if msg == TYPE_WD_BIZ_FAILED {
-			app.res.bizFailed++
-		} else if msg == TYPE_FAILED {
-			app.res.failed++
-		}
-	}
-}
-
-func (app *benchmarkApp) doOneWorker() {
-	//trans := app.transport
+func (wk *worker) startWorker() {
 	trans := &http.Transport{}
-	for {
-		app.doOneRequest(trans)
+	for !wk.StopFlag {
+		wk.doOneRequest(trans)
 	}
 }
 
-func (app *benchmarkApp) doOneRequest(trans *http.Transport) {
-	targetUrl := app.getTargetUrl(app.UrlG)
+func (wk *worker) doOneRequest(trans *http.Transport) {
+	targetUrl := wk.getTargetUrl(wk.UrlG)
 	client := &http.Client{Transport: trans}
 	req, _ := http.NewRequest(http.MethodGet, targetUrl, nil)
 	resp, err := client.Do(req)
 
 	if err != nil {
-		app.msgChan <- TYPE_FAILED
+		wk.res.failed++
 		return
 	}
 	if resp.StatusCode != 200 {
-		app.msgChan <- TYPE_FAILED
+		wk.res.failed++
 		return
 	}
 	r, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		app.msgChan <- TYPE_FAILED
+		wk.res.failed++
 	}
-	if app.param.Wd && !strings.HasPrefix(string(r), "{\"errno\":0") {
-		app.msgChan <- TYPE_WD_BIZ_FAILED
+	if wk.param.Wd && !strings.HasPrefix(string(r), "{\"errno\":0") {
+		wk.res.bizFailed++
 	}
-	app.msgChan <- TYPE_TOTAL
+	wk.res.total++
 }
 
-func (app *benchmarkApp) getTargetUrl(urlG UrlGenerator) string {
+func (wk *worker) getTargetUrl(urlG UrlGenerator) string {
 	return urlG.GetUrl()
 }
